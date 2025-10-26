bcc6e14ed26b4de1c24c342af1391edff01768dc
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-10-26 19:33:26 +0700

    Update Book.java

--- Praktikum4/Book.java
+++ Praktikum4/Book.java
@@ -1,10 +1,33 @@
 package Praktikum4;
-
 public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
 
-	}
+    public String getTitle() {
+        return title;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowBook() {
+        this.isAvailable = false;
+    }
+
+    public void returnBook() {
+        this.isAvailable = true;
+    }
 }
+
