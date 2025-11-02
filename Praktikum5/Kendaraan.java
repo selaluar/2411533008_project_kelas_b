ed7086b1cd7ab23e022babb621518b4a9adef648
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-11-02 19:19:56 +0700

    Change fields to private in Kendaraan class

--- Praktikum5/Kendaraan.java
+++ Praktikum5/Kendaraan.java
@@ -1,20 +1,25 @@
 package Praktikum5;
+
 public abstract class Kendaraan {
-    String merk;
-    String model;
-    int tahunProduksi;
+	private String merk;
+	private String model;
+	private int tahunProduksi;
+	
+	public Kendaraan (String merk, String model, int tahunProduksi) {
+		this.merk = merk;
+		this.model = model;
+		this.tahunProduksi = tahunProduksi;
+	}
+	
+	//abstract method
+	public abstract void nyalakanMesin();
+	
+	//method final 
+	public final void tampilkanInfo() {
+		System.out.println("Merek : " + this.merk);
+		System.out.println("Model : " + this.model);
+		System.out.println("Tahun Produksi : " + this.tahunProduksi);
+	}
+	
 
-    public Kendaraan(String merk, String model, int tahunProduksi) {
-        this.merk = merk;
-        this.model = model;
-        this.tahunProduksi = tahunProduksi;
-    }
-
-    public abstract void nyalakanMesin();
-
-    public final void tampilkanInfo() {
-        System.out.println("Merk: " + merk);
-        System.out.println("Model: " + model);
-        System.out.println("Tahun Produksi: " + tahunProduksi);
-    }
 }
