1265bca393565949a8157aad5640f6e01b51b911
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-11-02 19:19:17 +0700

    Refactor Bus class and update passenger capacity

--- Praktikum5/Bus.java
+++ Praktikum5/Bus.java
@@ -1,43 +1,43 @@
 package Praktikum5;
 public class Bus extends Kendaraan implements TransportasiUmum {
-    String kelasBus;
-
-    public Bus(String merk, String model, int tahunProduksi, String kelasBus) {
-        super(merk, model, tahunProduksi);
-        this.kelasBus = kelasBus;
+	private String kelasBus;
+	
+	public Bus (String merk, String model, int tahunProduksi, String kelasBus) {
+		super(merk, model, tahunProduksi);
+		this.kelasBus = kelasBus;
+	}
+	
+	public void nyalakanMesin() {
+        System.out.println("Nyalakan Mesin : Putar Kunci unutk menyalakan");
     }
-
-    @Override
-    public void nyalakanMesin() {
-        System.out.println("Bus dinyalakan menggunakan sistem starter diesel.");
+	
+	public String jenisBahanBakar() {
+		return "Solar";
+	}
+	
+	public int kapasitasPenumpang() {
+        return 45;
     }
-
-    @Override
-    public String jenisBahanBakar() {
-        return "Solar";
+	
+	public void fiturBus() {
+        System.out.println("Fitur bus: Dilengkapi kursi nyaman dan fasilitas hiburan (Wifi).");
     }
+	
+	//inner class
+	public class JadwalPerjalanan {
+		private String rute;
+		private String waktuBerangkat;
+		
+		public JadwalPerjalanan (String rute, String waktuBerangkat) {
+			this.rute = rute;
+			this.waktuBerangkat = waktuBerangkat;
+		}
+		
+		public void tampilkanJadwal() {
+			System.out.println("Rute perjalanan : " + rute + ", " + waktuBerangkat);
+		}
+	}
+	
 
-    @Override
-    public int kapasitasPenumpang() {
-        return 50;
-    }
-
-    public void fiturBus() {
-        System.out.println("Fitur: Toilet, Kursi Reclining, Wi-Fi.");
-    }
-
-    public class JadwalPerjalanan {
-        String rute;
-        String waktuBerangkat;
-
-        public JadwalPerjalanan(String rute, String waktuBerangkat) {
-            this.rute = rute;
-            this.waktuBerangkat = waktuBerangkat;
-        }
-
-        public void tampilkanJadwal() {
-            System.out.println("Rute: " + rute);
-            System.out.println("Waktu Berangkat: " + waktuBerangkat);
-        }
-    }
 }
+
