80d665c18c4ba5f3be12a7341494b382273020b7
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-11-02 19:21:16 +0700

    Refactor Mobil class with updated methods and features

--- Praktikum5/Mobil.java
+++ Praktikum5/Mobil.java
@@ -1,23 +1,22 @@
 package Praktikum5;
 public final class Mobil extends Kendaraan implements BahanBakar {
-    String jenisTransmisi;
-
-    public Mobil(String merk, String model, int tahunProduksi, String jenisTransmisi) {
-        super(merk, model, tahunProduksi);
-        this.jenisTransmisi = jenisTransmisi;
-    }
-
-    @Override
-    public void nyalakanMesin() {
-        System.out.println("Mobil dinyalakan dengan menekan tombol Start/Stop atau memutar kunci.");
-    }
-
-    @Override
-    public String jenisBahanBakar() {
-        return "Bensin";
-    }
-
-    public void fiturMobil() {
-        System.out.println("Fitur: AC, Airbag, Sensor Parkir.");
-    }
+	private String jenisTransmisi;
+	
+	public Mobil (String merk, String model, int tahunProduksi, String jenisTransmisi) {
+		super(merk, model, tahunProduksi);
+		this.jenisTransmisi = jenisTransmisi;
+	}
+	
+	public void nyalakanMesin() {
+		System.out.println("Nyalakan Mesin : Tekan tombol start engine");
+	}
+	
+	public String jenisBahanBakar() {
+		return "Bensin";
+	}
+	
+	public void fiturMobil() {
+		System.out.println("Fitur mobil: Air Conditioner, Sensor Parkir, dan audio premium.");		
+	}
+	
 }
