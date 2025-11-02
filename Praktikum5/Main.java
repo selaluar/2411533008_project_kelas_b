09edb329d861d5a15e9f472433b4fa400aeeff65
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-11-02 19:15:37 +0700

    Update Main.java

--- Praktikum5/Main.java
+++ Praktikum5/Main.java
@@ -1,34 +1,40 @@
 package Praktikum5;
 public class Main {
     public static void main(String[] args) {
-        Mobil mobil = new Mobil("Toyota", "Avanza", 2022, "Automatic");
-        Bus bus = new Bus("Mercedes-Benz", "OH 1526", 2020, "Eksekutif");
-        Pesawat pesawat = new Pesawat("Boeing", "737 Max", 2019, "Jet", "Garuda Indonesia");
 
-        System.out.println("=== Informasi Mobil ===");
+        Mobil mobil = new Mobil("Toyota", "Avanza", 2021, null);
+        
         mobil.tampilkanInfo();
         mobil.nyalakanMesin();
-        System.out.println("Bahan Bakar: " + mobil.jenisBahanBakar());
-        mobil.fiturMobil();
+        System.out.println("Bahan Bakar    :" + mobil.jenisBahanBakar());
         mobil.infoKonsumsi();
+        mobil.fiturMobil();
 
-        System.out.println("\n=== Informasi Bus ===");
+        System.out.println();
+
+        // === OBJEK BUS ===
+        Bus bus = new Bus("Mercedes–Benz", "Bus Pariwisata", 2018, null, 45);
         bus.tampilkanInfo();
         bus.nyalakanMesin();
-        System.out.println("Bahan Bakar: " + bus.jenisBahanBakar());
-        System.out.println("Kapasitas: " + bus.kapasitasPenumpang() + " Penumpang");
-        bus.fiturBus();
+        System.out.println("Bahan Bakar    : " + bus.jenisBahanBakar());
         bus.infoKonsumsi();
+        bus.tampilkanInfo();
+        bus.fiturBus();
 
-        Bus.JadwalPerjalanan jadwal = bus.new JadwalPerjalanan("Padang - Pekanbaru", "08:00");
+        // === INNER CLASS ===
+        Bus.JadwalPerjalanan jadwal = bus.new JadwalPerjalanan("Jakarta – Bandung", " 08:00");
         jadwal.tampilkanJadwal();
+        
+        System.out.println();
 
-        System.out.println("\n=== Informasi Pesawat ===");
+        // === OBJEK PESAWAT ===
+        Pesawat pesawat = new Pesawat("Boeing", "737 ", 2022, "Domestik", "Garuda Indonesia", 180);
         pesawat.tampilkanInfo();
         pesawat.nyalakanMesin();
-        System.out.println("Bahan Bakar: " + pesawat.jenisBahanBakar());
+        System.out.println("Bahan Bakar    : " + pesawat.jenisBahanBakar());
         pesawat.jenisPenerbangan();
-        System.out.println("Maskapai: " + pesawat.namaMaskapai());
-        pesawat.infoKonsumsi();
+        System.out.println("Maskapai       : " + pesawat.namaMaskapai());
+
     }
 }
+
